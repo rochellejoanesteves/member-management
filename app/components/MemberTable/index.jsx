66ac33cea385949579ac30a0ba93347e87367909
Rochelle <rochellejoan.p.esteves@gmail.com>
2024-05-02 21:23:05 +0800

    display members data on table

--- app/components/MemberTable/index.jsx
+++ app/components/MemberTable/index.jsx
@@ -1,8 +1,100 @@
+import { Table } from "antd";
 import React from "react";
 
 function MemberTable({ userDetails }) {
-  console.log(userDetails);
-  return <div></div>;
+  const userData = userDetails?.edges?.map((item, index) => ({
+    ...item.node,
+    key: index,
+  }));
+  const columns = [
+    {
+      title: "Name",
+      children: [
+        {
+          title: "Name",
+          dataIndex: "name",
+          key: "name",
+          render: (text) => <span style={{ color: "#f2bf4e" }}>{text}</span>,
+        },
+      ],
+    },
+    {
+      title: "Verification Status",
+      children: [
+        {
+          title: "Verification Status",
+          dataIndex: "verificationStatus",
+          key: "verificationStatus",
+        },
+      ],
+    },
+    {
+      title: "Email Address",
+      children: [
+        {
+          title: "Email Address",
+          dataIndex: "emailAddress",
+          key: "emailAddress",
+        },
+      ],
+    },
+    {
+      title: "Mobile Number",
+      children: [
+        {
+          title: "Mobile Number",
+          dataIndex: "mobileNumber",
+          key: "mobileNumber",
+        },
+      ],
+    },
+    {
+      title: "Domain",
+      children: [
+        {
+          title: "Domain",
+          dataIndex: "domain",
+          key: "domain",
+        },
+      ],
+    },
+    {
+      title: "Date Registered",
+      children: [
+        {
+          title: "Date Registered",
+          dataIndex: "dateTimeCreated",
+          key: "dateTimeCreated",
+        },
+      ],
+    },
+    {
+      title: "Status",
+      children: [
+        {
+          title: "Status",
+          dataIndex: "status",
+          key: "status",
+        },
+      ],
+    },
+    {
+      title: "Date and Time Last Active",
+      children: [
+        {
+          title: "Date and Time Last Active",
+          dataIndex: "dateTimeLastActive",
+          key: "dateTimeLastActive",
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div>
+      <Table columns={columns} dataSource={userData} pagination={false} />
+    </div>
+  );
 }
 
 export default MemberTable;
